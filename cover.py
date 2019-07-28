fa7b25c59516c36481a8baff11d16fc3b0b99681
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2019-07-28 09:50:36 +0800

    第二次更新

--- cover.py
+++ cover.py
@@ -1,7 +1,9 @@
 ﻿import urllib.request
 import zlib
 from lxml import etree
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 tar=input()
 req=urllib.request.Request(url='https://www.bilibili.com/video/av'+tar,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'})
 response=urllib.request.urlopen(req)
@@ -10,5 +12,5 @@ content=zlib.decompress(content, 16+zlib.MAX_WBITS)
 text=content.decode('utf-8') 
 tree = etree.HTML(text)
 result=tree.xpath('/html/head/meta[@itemprop="image"]/@content')
-urllib.request.urlretrieve(result[0],filename="D://"+tar+".jpg")
+urllib.request.urlretrieve(result[0],filename=tar+".jpg")
 print("已保存：av"+tar)
